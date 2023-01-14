62663d130f101691406c76fe1525ed1aece9ef28
rizaldiabyannata <aldizar255@gmail.com>
2023-01-14 19:11:30 +0800

    second commit

--- bot/views.py
+++ bot/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from threading import Thread
 from . import telegram_bot
 
@@ -6,4 +6,4 @@ def handle_telegram_bot(request):
     bot_thread = Thread(target=telegram_bot.run_bot)
     bot_thread.daemon = True
     bot_thread.start()
-    return HttpResponse("Bot running") 
+    return render(request, 'lynx/index.html')
